81689c18f082f4adf59551e861e11d327c0c741a
svpunchihewa <svpunchihewa@CMRCAMBRIDGE12.Virtusa.com>
2017-11-08 12:58:08 +0530

    Update

---
+++ Selenium10/src/Selenium10/Selenium10.java
@@ -0,0 +1,12 @@
+package Selenium10;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Selenium10 {
+	
+	WebDriver driver= new FirefoxDriver();
+	driver.get("http://www.google.lk;");
+	
+
+}
---
+++ Selenium_New10/src/NewSelenium/NewClass.java
@@ -0,0 +1,13 @@
+package NewSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NewClass {
+	
+	public static void main(String[] args)
+	{
+	WebDriver driver=new FirefoxDriver();
+	driver.get("http://www.google.lk");
+	}	
+}
